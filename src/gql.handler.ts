fea3d214ae163ecb3845194ee435309f90248444
frizadiga <frizadiga@gmail.com>
2024-11-10 12:39:37 +0700

    sync: batch

--- src/gql.handler.ts
+++ src/gql.handler.ts
@@ -8,30 +8,30 @@ const PATH_TO_MOCKS = '/Users/bytedance/Documents/main/mock-server/mocks';
 
 // determine mock to read
 async function find_mock() {
-    const mock_filename = 'scroogeRenderPaymentRegister.gql.example.json';
-    const mock_path = path.join(PATH_TO_MOCKS, mock_filename);
-    // const mock_content = await $`cat ${mock_path}`;
-    const mock_content = await Bun.file(mock_path, {}).text();
-    const mock_json = JSON.parse(mock_content);
+  const mock_filename = 'scroogeRenderPaymentRegister.gql.example.json';
+  const mock_path = path.join(PATH_TO_MOCKS, mock_filename);
+  // const mock_content = await $`cat ${mock_path}`;
+  const mock_content = await Bun.file(mock_path, {}).text();
+  const mock_json = JSON.parse(mock_content);
 
-    return mock_json;
+  return mock_json;
 }
 
 interface Mock {
-    metadata: unknown;
-    response: unknown;
+  metadata: unknown;
+  response: unknown;
 }
 
 function normalize_mock(mock: Mock) {
-    const normalized_mock = mock?.response;
-    return normalized_mock;
+  const normalized_mock = mock?.response;
+  return normalized_mock;
 }
 
 async function gql_handler() {
-    const final_mock = await find_mock();
-    const final_mock_normalized = normalize_mock(final_mock);
+  const final_mock = await find_mock();
+  const final_mock_normalized = normalize_mock(final_mock);
 
-    return json({ data: final_mock_normalized });
+  return json({ data: final_mock_normalized });
 }
 
 export { gql_handler }
